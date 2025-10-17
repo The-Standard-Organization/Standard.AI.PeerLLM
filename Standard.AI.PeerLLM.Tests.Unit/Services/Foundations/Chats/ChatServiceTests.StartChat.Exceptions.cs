36ef83f968a5501ac03191d034c0b52ccaa3e540
Christo du Toit <christo@dutoit.co.uk>
2025-10-17 20:19:19 +0100

    ShouldThrowDependencyValidationExceptionOnStartChatIfBadRequestAsync -> FAIL

---
+++ Standard.AI.PeerLLM.Tests.Unit/Services/Foundations/Chats/ChatServiceTests.StartChat.Exceptions.cs
@@ -0,0 +1,65 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Standard.AI.PeerLLM.Models.Foundations.Chats.Exceptions;
+
+namespace Standard.AI.PeerLLM.Tests.Unit.Services.Foundations.Chats
+{
+    public partial class ChatServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnStartChatIfBadRequestAsync()
+        {
+            // given
+            ChatSessionConfig someChatSessionConfig = CreateRandomChatSessionConfig();
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            var badRequestException =
+                new HttpRequestException(
+                    message: "Bad Request",
+                    inner: null,
+                    statusCode: System.Net.HttpStatusCode.BadRequest);
+
+            var hostNotFoundException =
+                new HostNotFoundException(
+                    message: "No hosts available for this model",
+                    innerException: badRequestException,
+                    data: badRequestException.Data);
+
+            var expectedChatDependencyValidationException =
+                new ChatDependencyValidationException(
+                    message: "Chat dependency validation error occurred, fix input and try again.",
+                    innerException: hostNotFoundException);
+
+            this.peerLLMBrokerMock.Setup(broker =>
+                broker.StartChatAsync(someChatSessionConfig, cancellationToken))
+                    .ThrowsAsync(badRequestException);
+
+            // when
+            ValueTask<Guid> startChatTask =
+                this.chatService.StartChatAsync(someChatSessionConfig, cancellationToken);
+
+            ChatDependencyValidationException actualChatDependencyValidationException =
+                await Assert.ThrowsAsync<ChatDependencyValidationException>(
+                    startChatTask.AsTask);
+
+            // then
+            actualChatDependencyValidationException.Should()
+                .BeEquivalentTo(expectedChatDependencyValidationException);
+
+            this.peerLLMBrokerMock.Verify(broker =>
+                broker.StartChatAsync(someChatSessionConfig, cancellationToken),
+                    Times.Once);
+
+            this.peerLLMBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
