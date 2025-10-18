f9335dbe98ff85017d0cbfe12bc1c79f39b9eb50
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 15:29:49 +0100

    ShouldThrowValidationExceptionOnStreamChatIfValidationErrorOccurredAsync -> FAIL

---
+++ Standard.AI.PeerLLM.Tests.Unit/Clients/Chats/ChatClientTests.StreamChat.Exceptions.cs
@@ -0,0 +1,56 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.PeerLLM.Models.Clients.Chats.Exceptions;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Xeptions;
+
+namespace Standard.AI.PeerLLM.Tests.Unit.Clients.Chats
+{
+    public partial class ChatClientTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldThrowValidationExceptionOnStreamChatIfValidationErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            ChatSessionConfig someChatSessionConfig = CreateRandomChatSessionConfig();
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            var expectedChatClientValidationException =
+                new ChatClientValidationException(
+                    message: "Chat client validation error occurred, fix errors and try again.",
+                    innerException: validationException.InnerException as Xeption,
+                    data: validationException.InnerException.Data);
+
+            this.chatServiceMock.Setup(broker =>
+                broker.StreamChatAsync(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                    .Throws(validationException);
+
+            // when
+            ValueTask<Guid> startChatTask =
+                this.chatClient.StartChatAsync(someChatSessionConfig, cancellationToken);
+
+            ChatClientValidationException actualChatClientDependencyException =
+                await Assert.ThrowsAsync<ChatClientValidationException>(
+                    startChatTask.AsTask);
+
+            // then
+            actualChatClientDependencyException.Should()
+                .BeEquivalentTo(expectedChatClientValidationException);
+
+            this.chatServiceMock.Verify(broker =>
+                broker.StreamChatAsync(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.chatServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
