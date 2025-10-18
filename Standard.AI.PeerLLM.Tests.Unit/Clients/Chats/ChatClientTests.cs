9f57856dcdb8311ee75b01093aa4a105a1647961
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 12:44:34 +0100

    ShouldStartChatAsync -> FAIL

---
+++ Standard.AI.PeerLLM.Tests.Unit/Clients/Chats/ChatClientTests.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Moq;
+using Standard.AI.PeerLLM.Clients.Chats;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Standard.AI.PeerLLM.Services.Foundations.Chats;
+using Tynamix.ObjectFiller;
+
+namespace Standard.AI.PeerLLM.Tests.Unit.Clients.Chats
+{
+    public partial class ChatClientTests
+    {
+        private readonly Mock<IChatService> chatServiceMock;
+        private readonly ChatClient chatClient;
+
+        public ChatClientTests()
+        {
+            this.chatServiceMock = new Mock<IChatService>();
+            this.chatClient = new ChatClient(chatService: this.chatServiceMock.Object);
+        }
+
+        private static ChatSessionConfig CreateRandomChatSessionConfig() =>
+            CreateChatSessionConfigFiller().Create();
+
+        private static Filler<ChatSessionConfig> CreateChatSessionConfigFiller()
+        {
+            var filler = new Filler<ChatSessionConfig>();
+            filler.Setup();
+
+            return filler;
+        }
+    }
+}
---
+++ Standard.AI.PeerLLM/Clients/Chats/ChatClient.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Standard.AI.PeerLLM.Services.Foundations.Chats;
+
+namespace Standard.AI.PeerLLM.Clients.Chats
+{
+    internal class ChatClient : IChatClient
+    {
+        private readonly IChatService chatService;
+
+        public ChatClient(IChatService chatService) =>
+            this.chatService = chatService;
+
+        public ValueTask<string> EndChatAsync(
+            Guid conversationId,
+            CancellationToken cancellationToken = default) =>
+                throw new NotImplementedException();
+
+        public ValueTask<Guid> StartChatAsync(
+            ChatSessionConfig chatSessionConfig,
+            CancellationToken cancellationToken = default) =>
+                throw new NotImplementedException();
+
+        public IAsyncEnumerable<string> StreamChatAsync(
+            Guid conversationId,
+            string text,
+            CancellationToken cancellationToken = default) =>
+                throw new NotImplementedException();
+    }
+}
