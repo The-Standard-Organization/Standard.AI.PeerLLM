6f5d1bf30cc03d04969da1b8121ee93e20175b0a
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 23:36:47 +0100

    CODE RUB: Updated build script

--- Standard.AI.PeerLLM.Infrastructure/Services/ScriptGenerationService.cs
+++ Standard.AI.PeerLLM.Infrastructure/Services/ScriptGenerationService.cs
@@ -73,8 +73,30 @@ namespace Standard.AI.PeerLLM.Infrastructure.Services
 
                                 new TestTask
                                 {
-                                    Name = "Test",
+                                    Name = "Run Unit Tests",
+                                    Shell = "pwsh",
+                                    Run =
+                                        """
+                                        $projects = Get-ChildItem -Path . -Filter "*Tests.Unit*.csproj" -Recurse
+                                        foreach ($project in $projects) {
+                                          Write-Host "Running tests for: $($project.FullName)"
+                                          dotnet test $project.FullName --no-build --verbosity normal
+                                        }
+                                        """
                                 },
+
+                                new TestTask
+                                {
+                                    Name = "Run Acceptance Tests",
+                                    Run =
+                                        """
+                                        $projects = Get-ChildItem -Path . -Filter "*Tests.Acceptance*.csproj" -Recurse
+                                        foreach ($project in $projects) {
+                                          Write-Host "Running tests for: $($project.FullName)"
+                                          dotnet test $project.FullName --no-build --verbosity normal
+                                        }
+                                        """
+                                }
                             }
                         }
                     },
