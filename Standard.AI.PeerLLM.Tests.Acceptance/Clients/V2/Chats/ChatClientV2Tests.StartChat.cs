a8076533df46617f7192931eb4b68510cc8cc033
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 21:33:01 +0100

    ShouldStartChat -> PASS

---
+++ Standard.AI.PeerLLM.Tests.Acceptance/Clients/V2/Chats/ChatClientV2Tests.StartChat.cs
@@ -0,0 +1,56 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace Standard.AI.PeerLLM.Tests.Acceptance.Clients.V2.Chats
+{
+    public partial class ChatClientV2Tests : IDisposable
+    {
+        [Fact]
+        public async Task ShouldStartChat()
+        {
+            // given
+            ChatSessionConfigV2 chatSessionConfig = new ChatSessionConfigV2
+            {
+                ModelName = GetRandomString(),
+                Role = GetRandomString(),
+                RoleContent = GetRandomString(),
+                TargetMachines = new List<string> { GetRandomString(), GetRandomString() },
+                FallBack = true,
+                AntiPrompts = new List<string> { GetRandomString(), GetRandomString() }
+            };
+
+            Guid conversationId = Guid.NewGuid();
+            Guid expectedConversationId = conversationId;
+            var expectedBody = JsonSerializer.Serialize(chatSessionConfig);
+
+            this.wireMockServer.Given(
+                Request.Create()
+                .UsingPost()
+                    .WithPath("/api/v2/chats/start")
+                    .WithHeader("Authorization", $"Bearer {this.apiKey}")
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithBody(expectedBody))
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(conversationId));
+
+            // when
+            Guid actualConversationId =
+                await this.peerLLMClient.V2.Chats.StartChatAsync(chatSessionConfig);
+
+            // then
+            actualConversationId.Should().Be(expectedConversationId);
+        }
+    }
+}
