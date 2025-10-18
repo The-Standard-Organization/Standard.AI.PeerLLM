579af70170f3cd3c8dfd9985266abfd71534fff6
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 21:23:38 +0100

    ShouldStreamChat -> PASS

---
+++ Standard.AI.PeerLLM.Tests.Acceptance/Clients/V1/Chats/ChatClientTests.StreamChat.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace Standard.AI.PeerLLM.Tests.Acceptance.Clients.V1.Chats
+{
+    public partial class ChatClientTests : IDisposable
+    {
+        [Fact]
+        public async Task ShouldStreamChat()
+        {
+            // given
+            Guid conversationId = Guid.NewGuid();
+            string text = GetRandomString();
+            Guid expectedConversationId = conversationId;
+            var expectedBody = JsonSerializer.Serialize(new { ConversationId = conversationId, Text = text });
+            CancellationToken cancellationToken = CancellationToken.None;
+            var expectedList = await ToListAsync(GetAsyncEnumerableOfRandomStrings(), cancellationToken);
+
+            this.wireMockServer.Given(
+                Request.Create()
+                .UsingPost()
+                    .WithPath("/api/chats/stream")
+                    .WithHeader("Authorization", $"Bearer {this.apiKey}")
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithBody(expectedBody))
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(expectedList));
+
+            // when
+            IAsyncEnumerable<string> actualResponse =
+                this.peerLLMClient.V1.Chats.StreamChatAsync(conversationId, text);
+
+            var actualList = await ToListAsync(actualResponse, cancellationToken);
+
+            // then
+            actualList.Should().Equal(expectedList);
+        }
+    }
+}
