993eaa69d1fe087432a53a9e7b6c75bc2d1afbac
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 23:52:25 +0100

    CODE RUB: Updated build script

--- Standard.AI.PeerLLM.Infrastructure/Services/ScriptGenerationService.cs
+++ Standard.AI.PeerLLM.Infrastructure/Services/ScriptGenerationService.cs
@@ -73,7 +73,15 @@ namespace Standard.AI.PeerLLM.Infrastructure.Services
 
                                 new TestTask
                                 {
-                                    Name = "Test"
+                                    Name = "Run Unit Tests",
+                                    Shell = "pwsh",
+                                    Run = "dotnet test **/*Tests.Unit.csproj --no-build --verbosity normal"
+                                },
+
+                                new TestTask
+                                {
+                                    Name = "Run Acceptance Tests",
+                                    Run = "dotnet test **/*Tests.Acceptance.csproj --no-build --verbosity normal"
                                 }
                             }
                         }
