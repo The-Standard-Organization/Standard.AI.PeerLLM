a549a7a8d58900821a8c6031dc61e5b069c47768
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 21:24:06 +0100

    ShouldEndChat -> PASS

---
+++ Standard.AI.PeerLLM.Tests.Acceptance/Clients/V1/Chats/ChatClientTests.EndChat.cs
@@ -0,0 +1,45 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace Standard.AI.PeerLLM.Tests.Acceptance.Clients.V1.Chats
+{
+    public partial class ChatClientTests : IDisposable
+    {
+        [Fact]
+        public async Task ShouldEndChat()
+        {
+            // given
+            Guid conversationId = Guid.NewGuid();
+            string expectedResponse = "Conversation ended successfully";
+            string expectedBody = JsonSerializer.Serialize(conversationId);
+
+            this.wireMockServer.Given(
+                Request.Create()
+                .UsingPost()
+                    .WithPath("/api/chats/end")
+                    .WithHeader("Authorization", $"Bearer {this.apiKey}")
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithBody(expectedBody)
+                    )
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(new { message = expectedResponse }));
+
+            // when
+            string actualResponse =
+                await this.peerLLMClient.V1.Chats.EndChatAsync(conversationId);
+
+            // then
+            actualResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+    }
+}
