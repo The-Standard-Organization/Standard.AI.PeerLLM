64d5adc3757248b7f0e368ac3c18f665be862aa9
Christo du Toit <christo@dutoit.co.uk>
2025-10-18 13:15:59 +0100

    ShouldThrowValidationExceptionOnStartChatIfValidationErrorOccurredAsync -> FAIL

---
+++ Standard.AI.PeerLLM.Tests.Unit/Clients/Chats/ChatClientTests.StartChat.Exceptions.cs
@@ -0,0 +1,56 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Standard.AI.PeerLLM.Models.Foundations.Chats.Exceptions;
+using Xeptions;
+
+namespace Standard.AI.PeerLLM.Tests.Unit.Clients.Chats
+{
+    public partial class ChatClientTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldThrowValidationExceptionOnStartChatIfValidationErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            ChatSessionConfig someChatSessionConfig = CreateRandomChatSessionConfig();
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            var expectedChatClientValidationException =
+                new ChatClientValidationException(
+                    message: "Chat client validation error occurred, fix errors and try again.",
+                    innerException: validationException,
+                    data: validationException.Data);
+
+            this.chatServiceMock.Setup(service =>
+                service.StartChatAsync(It.IsAny<ChatSessionConfig>(), It.IsAny<CancellationToken>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<Guid> startChatTask =
+                this.chatClient.StartChatAsync(someChatSessionConfig, cancellationToken);
+
+            ChatClientValidationException actualChatClientValidationException =
+                await Assert.ThrowsAsync<ChatClientValidationException>(
+                    startChatTask.AsTask);
+
+            // then
+            actualChatClientValidationException.Should()
+                .BeEquivalentTo(expectedChatClientValidationException);
+
+            this.chatServiceMock.Verify(service =>
+                service.StartChatAsync(It.IsAny<ChatSessionConfig>(), It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.chatServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Standard.AI.PeerLLM.Tests.Unit/Clients/Chats/ChatClientTests.cs
+++ Standard.AI.PeerLLM.Tests.Unit/Clients/Chats/ChatClientTests.cs
@@ -5,8 +5,10 @@
 using Moq;
 using Standard.AI.PeerLLM.Clients.Chats;
 using Standard.AI.PeerLLM.Models.Foundations.Chats;
+using Standard.AI.PeerLLM.Models.Foundations.Chats.Exceptions;
 using Standard.AI.PeerLLM.Services.Foundations.Chats;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace Standard.AI.PeerLLM.Tests.Unit.Clients.Chats
 {
@@ -20,6 +22,34 @@ namespace Standard.AI.PeerLLM.Tests.Unit.Clients.Chats
             this.chatServiceMock = new Mock<IChatService>();
             this.chatClient = new ChatClient(chatService: this.chatServiceMock.Object);
         }
+
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var someException = new Xeption(exceptionMessage);
+
+            someException.AddData(
+                key: "conversationId",
+                values: "Id is required");
+
+            return new TheoryData<Xeption>
+            {
+                new ChatValidationException(
+                    message: "Chat validation error occured, fix errors and try again",
+                    innerException: someException),
+
+                new ChatDependencyValidationException(
+                    message: "Chat dependency validation error occurred, fix errors and try again",
+                    innerException: someException)
+            };
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
 
         private static ChatSessionConfig CreateRandomChatSessionConfig() =>
             CreateChatSessionConfigFiller().Create();
